2705f897d519a7a17c9b5d3d102cba2ac89c66a0
wontae <beegramin9@gmail.com>
2021-02-14 10:54:11 +0900

    taskDB에 userDB가 연결되었음

--- Refactoring with DB/src/tasks/entity/task.entity.ts
+++ Refactoring with DB/src/tasks/entity/task.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/entity/user.entity";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { TaskStatus } from "./task-status.enum";
 
 @Entity()
@@ -12,4 +13,10 @@ export class Task extends BaseEntity {
     description: string;
     @Column()
     status: TaskStatus;
+    
+    @ManyToOne(type => User, user => user.tasks, { eager : false } )
+    user: User;
+    
+    @Column()
+    userId: number;
 }
