066a65fc9a7ce975541e2e1012362de3423ab28c
wontae <beegramin9@gmail.com>
2021-02-14 10:44:07 +0900

    /test url에 AuthGuard() 테스트(사실 불필요)

--- Refactoring with DB/src/auth/auth.controller.ts
+++ Refactoring with DB/src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorator/get-user.decorator';
+import { User } from './entity/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -16,7 +19,20 @@ export class AuthController {
     }
     
     @Post('/signin')
-    async singIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    async singIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialsDto);
+    }
+
+    // 밑은 정말 Test, 사실 필요하지 않다.
+    @Post('/test')
+    /* Authentication 메커니즘을 적용하기 위해선
+    콘트롤러에 메커니즘을 적용하거나
+    콘트롤러의 request마다 적용할 수 있음 */
+    @UseGuards(AuthGuard())
+    /* Body나 Header만 가져오는 게 아니라 Req 전체를 가져오고 싶음 */
+    test(@GetUser() user: User) {
+        /* 이전에는 @Req() req로 request 전체를 가져왔음
+        이제는 @GetUser라는 custom decorator를 만들어서 더 간단하게 */
+        console.log(user);
     }
 }
