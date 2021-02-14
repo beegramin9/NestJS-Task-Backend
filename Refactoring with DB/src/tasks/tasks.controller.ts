90e50fc5ea49f7c84e625f64824562d54009d1dd
wontae <beegramin9@gmail.com>
2021-02-14 10:49:35 +0900

    task module에 AuthGuard 추가

--- Refactoring with DB/src/tasks/tasks.controller.ts
+++ Refactoring with DB/src/tasks/tasks.controller.ts
@@ -1,50 +1,73 @@
-import { Body, Controller, Param, Get, Post, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Param, Get, Post, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, Logger } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { TaskStatusValidationPipe } from 'src/tasks/pipes/task-status-validation.pipe';
 import { Task } from './entity/task.entity';
 import { TaskStatus } from './entity/task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/auth/entity/user.entity';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
 
 
 @Controller('tasks')
+/* 이게 전체를 가드하는 방법 */
+@UseGuards(AuthGuard())
 export class TasksController {
+    /* 여기 선언하면 TasksController 클래스 안에서 모두 사용 가능  */
+    private logger = new Logger('TaskController');
+
     constructor(
         private tasksService: TasksService
     ) {}
 
     @Get()
-    getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Promise<Task[]> {
-        return this.tasksService.getTask(filterDto);
+    getTasks(
+        @Query(ValidationPipe) filterDto: GetTaskFilterDto,
+        @GetUser() user: User
+        ): Promise<Task[]> {
+        this.logger.verbose(`User "${user.username} retrieving all tasks. Filters: ${JSON.stringify(filterDto)}"`)
+        return this.tasksService.getTask(filterDto, user);
     }
     
     @Post()
     @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-        /* 여긴 잘 들어와. 숫자가 잘 들어오네 */
-        return this.tasksService.createTask(createTaskDto);
+    createTask(
+        @Body() createTaskDto: CreateTaskDto,
+        /* 내가 만든 custom decorator  */
+        @GetUser() user: User,
+        ): Promise<Task> {
+        this.logger.verbose(`User "${user.username} creating a new task. Data: ${JSON.stringify(createTaskDto)}"`)
+        return this.tasksService.createTask(createTaskDto, user);
     }
 
     @Get('/:id')
     /* id: number 에서 끝나면 TypeScript이 compile할 때만 먹히는 것
     실제 돌아갈 때(runtime)는 ParseIntPipe로 실제로 바꿔줘야 함 */
-    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    getTaskById(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User
+        ): Promise<Task> {
         /* 이건 숫자가 안 들어와, 아예 실행도 안 되는거야 */
-        return this.tasksService.getTaskById(id);
+        return this.tasksService.getTaskById(id, user);
     }
 
     /* parseIntPipe이 안 먹혀서 그런가? */
     @Delete('/:id')
-    deleteTaskById(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.tasksService.deleteTaskById(id);
+    deleteTaskById(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User
+        ): Promise<void> {
+        return this.tasksService.deleteTaskById(id, user);
     }
 
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id', ParseIntPipe) id: number,
-        @Body('status', TaskStatusValidationPipe) status: TaskStatus
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User
     ): Promise<Task> {
-        return this.tasksService.updateTaskStatus(id, status);
+        return this.tasksService.updateTaskStatus(id, status, user);
     }
 
 }
--- Refactoring with DB/src/tasks/tasks.service.ts
+++ Refactoring with DB/src/tasks/tasks.service.ts
@@ -4,6 +4,7 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { TaskRepository } from './repository/task.repository';
 import { Task } from './entity/task.entity';
+import { User } from '../auth/entity/user.entity';
 import { TaskStatus } from './entity/task-status.enum';
 
 
@@ -14,34 +15,53 @@ export class TasksService {
         private taskRepository: TaskRepository,
     ) {}
 
-    async getTask(filterDto: GetTaskFilterDto): Promise<Task[]> {
-        return this.taskRepository.getTasks(filterDto);
+    async getTask(
+        filterDto: GetTaskFilterDto,
+        user: User
+        ): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto, user);
     }
 
 
-    async getTaskById(id: number): Promise<Task> {
-        const found = await this.taskRepository.findOne(id);
+    async getTaskById(
+        id: number,
+        user: User
+        ): Promise<Task> {
+            /* findOne 안에 오브젝트를 줘서 쿼리를 선택 가능(여기선 where절) */
+        const found = await this.taskRepository.findOne( { where: { id, userId: user.id } } );
         if (!found) {
             throw new NotFoundException(`Task with ID ${id} not found.`);
         }
         return found;
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-        return await this.taskRepository.createTask(createTaskDto);
+    async createTask(
+        createTaskDto: CreateTaskDto,
+        user: User
+        ): Promise<Task> {
+        return await this.taskRepository.createTask(createTaskDto, user);
     }
 
-    async deleteTaskById(id: number): Promise<void> {
-        const result = await this.taskRepository.delete(id);
+    async deleteTaskById(
+        id: number,
+        user: User
+        ): Promise<void> {
+            /* delete도 findOne처럼 Object로 파라미터를 받을 수 있다
+            근데 where처럼 query를 명시하진 않는다. 걍 문법이 그렇게 되어있음  */
+        const result = await this.taskRepository.delete({ id, userId: user.id} );
         /* the number of affected, 0이면 하나도 안 지워진거고 1이상이면 몇개 지워진거 */
         if (result.affected === 0) {
             throw new NotFoundException(`Task with ID ${id} not found.`);
         }
     }
 
-    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    async updateTaskStatus(
+        id: number, 
+        status: TaskStatus,
+        user: User
+        ): Promise<Task> {
         /* getTaskById를 한 번 더 사용하면 오류도 알아서 throw한다. */
-        const task = await this.getTaskById(id);
+        const task = await this.getTaskById(id, user);
         task.status = status;
         /* update를 할 때 요건 국지적으로 요 안에 있는 task에만 적용되므로
         따로 save() 해줘야 함. 모든 entity(여기선 Task)엔 save()이 있음. async operation임 */
