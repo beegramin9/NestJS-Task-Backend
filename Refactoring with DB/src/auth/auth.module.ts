209eda462d99c03205646419d77ca4b5e8022241
wontae <beegramin9@gmail.com>
2021-02-14 10:44:53 +0900

    AuthService, JwtStrategy, Configuration 추가

--- Refactoring with DB/src/auth/auth.module.ts
+++ Refactoring with DB/src/auth/auth.module.ts
@@ -5,6 +5,12 @@ import { AuthService } from './auth.service';
 import { UserRepository } from './repository/user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt/jwt-strategy';
+import { TasksModule } from 'src/tasks/tasks.module';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
+
 
 @Module({
   imports: [
@@ -13,14 +19,25 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([UserRepository]),
     /* 나중에 package.json 가져와서 npm install 한다해도 여기서 JwtModule import 해야 함! */
     JwtModule.register({ /* register로 call하고 configuration 줘야 함 */
-      secret: 'topSecret51',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600 /* 1Hr */
+        expiresIn: jwtConfig.expiresIn 
       }
     }),
     PassportModule.register({defaultStrategy:'jwt'})
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  /* provider에는 그냥 service가 들어오는 곳이라고 생각하면 편하다. */
+  providers: [
+    AuthService,
+    JwtStrategy
+  ],
+  /* exports로 들어가면, AuthModule 말고도 다른 모듈(TaskModule)
+  에서도 사용 가능
+  AuthModule에서 export된 모듈을 TaskModule에서 import해서 사용  */
+  exports: [
+    JwtStrategy,
+    PassportModule
+  ]
 })
 export class AuthModule {}
