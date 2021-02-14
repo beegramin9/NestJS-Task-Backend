0e03b1efe8580643d380c2a5d014207f33bd08b8
wontae <beegramin9@gmail.com>
2021-02-14 10:45:33 +0900

    유저의 payload 받아서 accessToken 받기

--- Refactoring with DB/src/auth/auth.service.ts
+++ Refactoring with DB/src/auth/auth.service.ts
@@ -1,29 +1,45 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './repository/user.repository';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+    private logger = new Logger('AuthService');
+
     /* 디펜던시 인젝션으로 UserRepository 사용하기 위해서는
     contstuctor 안에 해야 함 */
     constructor(
         @InjectRepository(UserRepository)
         /* private을 하면 이 클래스 내에서만 쓸 수 있게 만들지만
         그와 동시에 userRepository를 클래스 멤버로 만드는 것과 같음 */
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        /* 요 jwtService는 auth.module.ts에서 import된 놈을 export해줬으니
+        데코레이터가 필요 없는 듯 하네. auth.module.ts에서는 
+        디펜던시 인젝션을 자동으로 해주는구나 */
+        private jwtService: JwtService
     ) {}
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto) {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         const username = await this.userRepository.validateUserPassword(authCredentialsDto);
-        
         if (!username) /* null */ {
             /* 해커들이 못 알아채도록 ID가 틀렸는지 PW가 틀렸는지 말 안해줌 */
             throw new UnauthorizedException('Invalid credentials.');
         }
+        /* 일단은 username만 넣기, jwtPayload의 타입을 마치 DTO처럼 정해놓음 */
+        const payload: JwtPayload = { username };
+        
+        /* 여기서 에러발생. jwtService에 에러가 있는거같은데... */
+        const accessToken = await this.jwtService.sign(payload);
+
+        this.logger.debug(`Generated JWT Token with payload ${JSON.stringify(payload)}`);
+
+        return { accessToken }
     }
 }
